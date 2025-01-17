82e6c3698d4ba4de3e6d2e8a5cadbbc032b486cd
Phelix <phelixmbani@gmail.com>
2025-01-17 18:41:32 +0300

    chatbox

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from sqlalchemy import asc
 
 from models import db, Message
 
@@ -14,13 +15,67 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        message = [message.to_dict() for message in Message.query.order_by(asc(Message.created_at)).all()]
+        response = make_response(
+            message,
+            200
+        )
+        return response
+    elif request.method == 'POST':
+       
+        data = request.get_json()
+        
+        if not data or 'body' not in data or 'username' not in data:
+            return ({"error": "Invalid input"}, 400)
+        new_message = Message(
+            body= data['body'],
+            username=data['username'],
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        
+        return make_response(jsonify(new_message.to_dict()),201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    
+    message = db.session.get(Message, id)
+    
+    if not message:
+        return make_response({'error': f'message with the id {id} not found'}, 404)
+ 
+    if request.method == 'PATCH':
+       data = request.get_json()
+       if not data:
+           return ({"error": "Invalid input"})
+       
+       for attr, value in data.items():
+           if hasattr(message, attr):
+               setattr(message, attr, value)
+           else:
+               return make_response({'error': 'Invalid attribute {attr}'}, 400)
+       db.session.commit()   
+        
+        
+       return make_response(jsonify(message.to_dict()), 200)
+   
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        
+        response_body = {
+            "Delete successful": True,
+            "message": "Message deleted"
+        }
+        response = make_response(
+            response_body,
+            200
+        )
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
